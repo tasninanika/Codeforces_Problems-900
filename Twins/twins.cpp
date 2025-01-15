ca14073ff5331fa90fa724b49fa47552e27e9876
anikatasnin <jarintasnin09@gmail.com>
2025-01-15 14:56:11 +0600

    Added Twins

--- Twins/twins.cpp
+++ Twins/twins.cpp
@@ -2,29 +2,32 @@
 using namespace std;
 
 int main(){
-    int n, sum = 0, sum_of_coins, sum_of_my_coins = 0, c = 0;
+    int n;
     cin >> n;
+
+    int sum = 0, sum_of_my_coins = 0, c = 0;
 
     int arr[n];
     for(int i = 0; i < n; i++){
         cin >> arr[i];
+        sum += arr[i];
     }
 
-    for(int i = 0; i < n; i++){
-        if(arr[i] < arr[i+1]){
-            swap(arr[i],arr[i+1]);
+    sum = sum / 2;
+
+    for(int i = 0; i < n-1; i++){
+        for(int j = 0; j < n-i-1; j++){
+            if(arr[j] < arr[j+1]){
+            swap(arr[j],arr[j+1]);
+            }
         }
     }
-    for(int i = 0; i < n; i++){
-        sum += arr[i];
-        sum_of_coins = sum;
-    }
-    for(int i = 0; i < n; i++){
 
-        if(sum_of_coins >= sum_of_my_coins){
-            sum_of_my_coins += arr[i];
-            sum_of_coins -= arr[i];
-            c++;
+    for (int i = 0; i < n; i++) {
+        sum_of_my_coins += arr[i];
+        c++;
+        if (sum_of_my_coins > sum) {
+            break;
         }
     }
     cout << c << endl;
