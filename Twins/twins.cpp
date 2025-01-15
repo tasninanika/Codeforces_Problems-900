bc72fe8b3a8c34147c21ae423a2d67cf4a8b2a43
anikatasnin <jarintasnin09@gmail.com>
2025-01-15 11:08:05 +0600

    Added Twins

--- Twins/twins.cpp
+++ Twins/twins.cpp
@@ -2,12 +2,16 @@
 using namespace std;
 
 int main(){
-    int n;
+    int n, sum = 0;
     cin >> n;
 
     int arr[n];
     for(int i = 0; i < n; i++){
         cin >> arr[i];
+    }
+
+    for(int i = 0; i < n; i++){
+        sum += arr[i];
     }
 
     return 0;
