bd2a1f44494ab9460e13674b51ba35f36e161696
anikatasnin <jarintasnin09@gmail.com>
2025-01-18 11:25:23 +0600

    Added HQ9+

--- HQ9+/hq9+.cpp
+++ HQ9+/hq9+.cpp
@@ -5,8 +5,12 @@ int main(){
     string p;
     cin >> p;
 
-    for(int i = 0; i < p.size(); i++){
+    int c = 0;
 
+    for(int i = 0; i < p.size(); i++){
+        if(p[i] == "H" || p[i] == "Q" || p[i] == "9" || p[i] == "+"){
+            c++;
+        }
     }
 
     return 0;
