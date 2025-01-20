d2ba22560e055db286a251d713f383eeef7d041a
anikatasnin <jarintasnin09@gmail.com>
2025-01-20 15:02:45 +0600

    Added Gravity Flip

--- Gravity Flip/gravityflip.cpp
+++ Gravity Flip/gravityflip.cpp
@@ -12,7 +12,9 @@ int main(){
 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n-i-1; j++){
-
+            if(arr[j] > arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
         }
     }
 
