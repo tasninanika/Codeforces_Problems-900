50341f0a10ec23faf96801f748490b6802936bfb
anikatasnin <jarintasnin09@gmail.com>
2025-01-25 09:47:53 +0600

    Added Dubstep

--- Dubstep/dubstep.cpp
+++ Dubstep/dubstep.cpp
@@ -5,6 +5,16 @@ int main(){
     string s;
     cin >> s;
 
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] != 'W' && s[i+1] != 'U' && s[i+2] != 'B'){
+            cout << s[i];
+        }
+        else{
+            i = i + 2;
+            cout << " ";
+        }
+    }
+
 
     return 0;
 }
