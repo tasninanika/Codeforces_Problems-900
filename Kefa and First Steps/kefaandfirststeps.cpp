76e986c5cefb779e92633d76a6227c760ca02cd0
anikatasnin <jarintasnin09@gmail.com>
2025-01-26 09:43:02 +0600

    added Kefa and First Steps

--- Kefa and First Steps/kefaandfirststeps.cpp
+++ Kefa and First Steps/kefaandfirststeps.cpp
@@ -5,9 +5,19 @@ int main(){
     long long n;
     cin >> n;
 
+    int c = 0;
     long long a[n];
     for(int i = 0; i < n; i++){
         cin >> a[i];
+    }
+
+    for(int i = 0; i < n; i++){
+        if(a[i] <= a[i+1]){
+            c++;
+        }
+        else{
+            c = 0;
+        }
     }
 
     return 0;
