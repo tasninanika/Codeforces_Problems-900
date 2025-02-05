4bb7a80706f8522649a223d6219f9392740a871f
anikatasnin <jarintasnin09@gmail.com>
2025-02-05 09:12:13 +0600

    Added Puzzles

--- Puzzles/puzzles.cpp
+++ Puzzles/puzzles.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int n, m;
+    int n, m, a = 0;
     cin >> n >> m;
 
     int f[m];
@@ -18,9 +18,9 @@ int main(){
         }
     }
 
-    int a = f[m-1] - f[0];
+    a = f[m-1] - f[0];
 
-    for(int i = 0; i < m; i++){
+    for(int i = n; i <= m; i++){
         int a = f[i+1] - f[i];
         if( < a)
     }
