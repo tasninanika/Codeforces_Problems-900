23aeb58d24d43260e5c1f0ca57ba2f0c0e8fb37f
anikatasnin <jarintasnin09@gmail.com>
2025-02-28 11:19:54 +0600

    added Game With Sticks

--- Game With Sticks/gamewithsticks.cpp
+++ Game With Sticks/gamewithsticks.cpp
@@ -9,6 +9,9 @@ int main(){
     if(n % 2 != 0 && m % 2 != 0){
         cout << "Akshat" << endl;
     }
+    else if (n == 1 || m == 1){
+        cout << "Akshat" << endl;
+    }
     else{
         cout << "Malvika" << endl;
     }
