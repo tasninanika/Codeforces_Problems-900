c1c5a404abf1423b9eb483ea3ced759766742a26
anikatasnin <jarintasnin09@gmail.com>
2025-07-29 20:32:52 +0600

    added Expensive Number

--- Expensive Number/expensiveNumber.cpp
+++ Expensive Number/expensiveNumber.cpp
@@ -1,1 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string numberString;
+        cin >> numberString;
+
+        int maxSequence = 0;
+        int zeroCount = 0;
+
+        for (int i = 0; i < (int)numberString.size(); i++) {
+            if (numberString[i] == '0') {
+                zeroCount++;
+            } else {
+                if (zeroCount + 1 > maxSequence) {
+                    maxSequence = zeroCount + 1;
+                }
+            }
+        }
+
+        cout << (int)numberString.size() - maxSequence << endl;
+    }
+
+    return 0;
+}
 
