80a9c51794dd42e8a424a28284e264990420444e
anikatasnin <jarintasnin09@gmail.com>
2025-10-07 20:54:48 +0600

    added Candies

--- Candies/candies.cpp
+++ Candies/candies.cpp
@@ -12,6 +12,7 @@ int main(){
         for (int k = 3; k <= n; k = k * 2 + 1) {
             if (n % k == 0) {
                 cout << n / k << endl;
+                break;
             }
         }
     }
