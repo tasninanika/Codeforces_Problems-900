3c2a639da23671af67e569a09df618100dd8f47d
anikatasnin <jarintasnin09@gmail.com>
2025-10-15 23:24:13 +0600

    added Two-gram

--- Two-gram/two-gram.cpp
+++ Two-gram/two-gram.cpp
@@ -1,1 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int n;
+    string s;
+    cin >> n >> s;
+
+    map<string, int> freq;
+    string ans;
+    int m = 0;
+
+    for(int i = 0; i < n - 1; i++){
+        string sub = s.substr(i, 2);
+        freq[sub]++;
+
+        if (freq[sub] > m) {
+            m = freq[sub];
+            ans = sub;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
