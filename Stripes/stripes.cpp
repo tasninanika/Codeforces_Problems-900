478cd16f5e08eafa8d06b4e908bdc92823c71d51
anikatasnin <jarintasnin09@gmail.com>
2025-10-31 11:11:35 +0600

    added Stripes

--- Stripes/stripes.cpp
+++ Stripes/stripes.cpp
@@ -13,7 +13,7 @@ int main(){
         for(int i = 0; i < 8; i++){
             bool red_win = true;
             for(int j = 0; j < 8; j++){
-
+                if(grid[i][j] != 'R'){
             }
         }
     }
