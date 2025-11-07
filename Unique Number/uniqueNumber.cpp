6d30c5938f2e1316d38b7af03cd3a03b8210418b
anikatasnin <jarintasnin09@gmail.com>
2025-11-07 10:38:25 +0600

    added Unique Number

--- Unique Number/uniqueNumber.cpp
+++ Unique Number/uniqueNumber.cpp
@@ -1,1 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int x;
+        cin >> x;
+
+        if(x > 45){
+            cout << -1 << endl;
+            return;
+        }
+
+        string result = "";
+        int current_sum = x;
+
+        for(int digit = 9; digit >= 1; digit--){
+            if(current_sum >= digit){
+                result = to_string(digit) + result;
+                current_sum -= digit;
+            }
+
+            if(current_sum == 0){
+                break;
+            }
+        }
+
+        if(current_sum == 0){
+            cout << result << endl;
+        }
+        else{
+            cout << -1 << endl;
+        }
+    }
+    return 0;
+}
