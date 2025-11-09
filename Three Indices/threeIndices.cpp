b36be0993af30501fec9485f93bcdf00047fa024
anikatasnin <jarintasnin09@gmail.com>
2025-11-09 09:58:05 +0600

    added Three Indices

--- Three Indices/threeIndices.cpp
+++ Three Indices/threeIndices.cpp
@@ -1,1 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+
+        for(int j = 1; j < n - 1; j++){
+            if(a[j] > a[j - 1] && a[j] > a[j + 1]){
+                cout << "YES\n";
+                cout << j << " " << j + 1 << " " << j + 2 << "\n";
+                continue;
+            }
+        }
+
+    }
+
+    cout << "NO\n";
+
+    return 0;
+}
