eb1f9f7dccc39ba2b65844eb97c7acc3caacd9ce
anikatasnin <jarintasnin09@gmail.com>
2025-11-13 11:58:17 +0600

    added Unnatural Language Processing

--- Unnatural Language Processing/unnaturalLanguageProcessing.cpp
+++ Unnatural Language Processing/unnaturalLanguageProcessing.cpp
@@ -22,6 +22,7 @@ int main(){
         while(i >= 0){
             if(i - 2 >= 0 && !is_vowel(s[i - 2]) && is_vowel(s[i - 1])){
                 result += s[i];
+                result += s[i - 1];
             }
         }
     }
