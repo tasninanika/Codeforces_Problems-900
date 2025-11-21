79946b9c932013a9f3492f531d53a0e39dbf7bcd
anikatasnin <jarintasnin09@gmail.com>
2025-11-21 23:44:26 +0600

    added AB Balance

--- AB Balance/ABBalance.cpp
+++ AB Balance/ABBalance.cpp
@@ -1,1 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        if (s.front() != s.back()) {
+            s.front() = s.back();
+        }
+
+        cout << s << "\n";
+    }
+
+    return 0;
+}
