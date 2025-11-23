4c6ab8e1cc0b6bce355c7b92483db7cf5a1f008a
anikatasnin <jarintasnin09@gmail.com>
2025-11-23 17:26:03 +0600

    added Food Buying

--- Food Buying/foodBuying.cpp
+++ Food Buying/foodBuying.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        long long s;
+        cin >> s;
+
+        long long totalSpent = 0;
+
+        while(s >= 10){
+            long long x = s / 10 * 10;
+
+            totalSpent += x;
+            s = (s % 10) + (s / 10);
+        }
+
+        totalSpent += s;
+
+        cout << totalSpent << "\n";
+    }
+
+    return 0;
+}
