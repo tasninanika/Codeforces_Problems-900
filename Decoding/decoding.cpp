f0fc6473fd3cdf8e4a382ee5a02388e25dac6588
anikatasnin <jarintasnin09@gmail.com>
2025-11-27 11:09:06 +0600

    added Decoding

--- Decoding/decoding.cpp
+++ Decoding/decoding.cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    string decoded = "";
+
+    for(int i = 0; i < n; i++){
+        if((n - i) % 2 != 0){
+            decoded += s[i];
+        }
+        else{
+            decoded = s[i] + decoded;
+        }
+    }
+
+    cout << decoded << endl;
+
+    return 0;
+}
