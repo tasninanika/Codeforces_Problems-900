99a5f8ced95da4a7df75e2310531356d485b7113
anikatasnin <jarintasnin09@gmail.com>
2025-11-28 22:43:33 +0600

    added NIT Destroys the Universe

--- NIT Destroys the Universe/NITDestroysTheUniverse.cpp
+++ NIT Destroys the Universe/NITDestroysTheUniverse.cpp
@@ -1,1 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+
+        int groups = 0;
+        for(int i = 0; i < n; i++){
+            if(a[i] != 0){
+                if(i == 0 || a[i - 1] == 0){
+                    groups++;
+                }
+            }
+        }
+
+        cout << min(groups, 2) << endl;
+    }
+
+    return 0;
+}
