7e7996b5bae6447a09832743a75a7e3ae077d893
anikatasnin <jarintasnin09@gmail.com>
2025-11-29 12:55:42 +0600

    added Game of Mathletes

--- Game of Mathletes/gameOfMathletes.cpp
+++ Game of Mathletes/gameOfMathletes.cpp
@@ -6,7 +6,36 @@ int main(){
     cin >> t;
 
     while(t--){
+        int n;
+        long long k;
+        cin >> n >> k;
 
+        vector<int> a(n);
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+
+        sort(a.begin(), a.end());
+
+        int l = 0;
+        int r = n - 1;
+        int ans = 0;
+
+        while(l < r){
+            long long sum = (long long)a[l] + a[r];
+            if(sum == k){
+                ans++;
+                l++;
+                r--;
+            }
+            else if(sum < k){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        cout << ans << "\n";
     }
 
 
