2fcc0d4ce6ec6f8bf8a3dabd3e81261f3102e254
anikatasnin <jarintasnin09@gmail.com>
2025-11-30 18:17:55 +0600

    added Tricky Sum

--- Tricky Sum/trickySum.cpp
+++ Tricky Sum/trickySum.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        long long n;
+        cin >> n;
+
+        long long total_sum = n * (n + 1) / 2;
+        long long current_power = 1;
+        long long sum_powers_of_two = 0;
+
+        while(current_power <= n){
+            sum_powers_of_two += current_power;
+            current_power *= 2;
+        }
+
+        cout << total_sum - 2 * sum_powers_of_two << endl;
+    }
+
+    return 0;
+}
