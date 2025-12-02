1db644a4f704cb42dbce81679620be9ade646f7e
anikatasnin <jarintasnin09@gmail.com>
2025-12-02 10:23:37 +0600

    added Nastya and Rice

--- Nastya and Rice/nastyaAndRice.cpp
+++ Nastya and Rice/nastyaAndRice.cpp
@@ -6,7 +6,18 @@ int main(){
     cin >> t;
 
     while(t--){
+        long long n, a, b, c, d;
+        cin >> n >> a >> b >> c >> d;
 
+        long long min_possible = n * (a - b);
+        long long max_possible = n * (a + b);
+
+        if (max_possible >= c && min_possible <= d) {
+            cout << "Yes" << "\n";
+        }
+        else {
+            cout << "No" << "\n";
+        }
     }
 
 
