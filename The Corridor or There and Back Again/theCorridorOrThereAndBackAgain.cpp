8965c4d3ee35146700069320c5b9da6f3c6b6dc5
anikatasnin <jarintasnin09@gmail.com>
2025-12-03 18:11:35 +0600

    added The Corridor or There and Back Again

--- The Corridor or There and Back Again/theCorridorOrThereAndBackAgain.cpp
+++ The Corridor or There and Back Again/theCorridorOrThereAndBackAgain.cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        int max_reach = 1000000;
+
+        for(int i = 0; i < n; i++){
+            int d, s;
+            cin >> d >> s;
+
+            int limit = d + (s - 1) / 2;
+            max_reach = min(max_reach, limit);
+        }
+
+        cout << max_reach << endl;
+    }
+    return 0;
+}
