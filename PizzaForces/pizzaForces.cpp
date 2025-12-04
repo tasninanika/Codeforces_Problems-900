8f0c62f9b5df5c4c582c21efce35ca2ba40f24b6
anikatasnin <jarintasnin09@gmail.com>
2025-12-04 18:04:36 +0600

    added PizzaForces

--- PizzaForces/pizzaForces.cpp
+++ PizzaForces/pizzaForces.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        long long n;
+        cin >> n;
+
+        if(n < 6){
+            cout << 15 << endl;
+        }
+        else{
+            if(n % 2 != 0){
+                n++;
+            }
+            cout << (n / 2) * 5 << endl;
+        }
+    }
+    return 0;
+}
