5e506c5f7a741e10876c9a729464e24f2f41bbf7
anikatasnin <jarintasnin09@gmail.com>
2025-12-06 08:47:00 +0600

    added Promo

--- Promo/promo.cpp
+++ Promo/promo.cpp
@@ -1,23 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main(){
     int n, q;
     cin >> n >> q;
 
     vector<long long> p(n);
-    for (int i = 0; i < n; ++i) {
+    for(int i = 0; i < n; i++){
         cin >> p[i];
     }
 
     sort(p.rbegin(), p.rend());
 
     vector<long long> pref(n + 1, 0);
-    for (int i = 0; i < n; ++i) {
+    for(int i = 0; i < n; i++){
         pref[i + 1] = pref[i] + p[i];
     }
 
-    while (q--) {
+    while(q--){
         int x, y;
         cin >> x >> y;
         cout << pref[x] - pref[x - y] << "\n";
