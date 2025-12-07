3817ae266258733a0816329117a0245fa2e2855e
anikatasnin <jarintasnin09@gmail.com>
2025-12-07 18:40:27 +0600

    added Having Been a Treasurer in the Past, I Help Goblins Deceive

--- Having Been a Treasurer in the Past, I Help Goblins Deceive/havingBeenaTreasurerInThePast,IHelpGoblinsDeceive.cpp
+++ Having Been a Treasurer in the Past, I Help Goblins Deceive/havingBeenaTreasurerInThePast,IHelpGoblinsDeceive.cpp
@@ -1,1 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        long long dashes = 0;
+        long long underscores = 0;
+
+        for(char c : s){
+            if(c == '-'){
+                dashes++;
+            }
+            else{
+                underscores++;
+            }
+        }
+
+        long long left = dashes / 2;
+        long long right = dashes - left;
+
+        cout << underscores * left * right << "\n";
+    }
+    return 0;
+}
