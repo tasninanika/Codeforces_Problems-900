c07cd2c903d9d14e7b1ce7e280e4d3185ba2af7f
anikatasnin <jarintasnin09@gmail.com>
2025-12-11 18:40:44 +0600

    added Restore the Weather

--- Restore the Weather/restoreTheWeather.cpp
+++ Restore the Weather/restoreTheWeather.cpp
@@ -1,1 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    long t;
+    cin >> t;
+
+    while(t--){
+        long n, k;
+        cin >> n >> k;
+
+        vector<pair<long, long>> a(n);
+        for(long p = 0; p < n; p++){
+            long x;
+            cin >> x;
+
+            a[p] = {x, p};
+        }
+
+        vector<long> b(n);
+        for(long p = 0; p < n; p++){
+            cin >> b[p];
+        }
+
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+
+        vector<long> c(n);
+        for(long p = 0; p < n; p++){
+            c[a[p].second] = b[p];
+        }
+
+        for(long p = 0; p < n; p++){
+            cout << c[p] << (p == n - 1 ? "" : " ");
+        }
+        cout << "\n";
+    }
+    return 0;
+}
