bc238b5e0bca7776776ae0c136ad4c0092e57d48
anikatasnin <jarintasnin09@gmail.com>
2025-12-12 21:44:01 +0600

    added Luntik and Subsequences

--- Luntik and Subsequences/luntikAndSubsequences.cpp
+++ Luntik and Subsequences/luntikAndSubsequences.cpp
@@ -1,1 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        long long c1 = 0;
+        long long c0 = 0;
+
+        for(int i = 0; i < n; i++){
+            int a;
+            cin >> a;
+
+            if(a == 1){
+                c1++;
+            }
+            else if(a == 0){
+                c0++;
+            }
+        }
+
+        long long ans = c1 * (1LL << c0);
+
+        cout << ans << endl;
+    }
+    return 0;
+}
