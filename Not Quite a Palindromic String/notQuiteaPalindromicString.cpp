f6c64ad2bda3ffff7420193d671cd0bd4608044c
anikatasnin <jarintasnin09@gmail.com>
2025-12-15 23:15:46 +0600

    added Not Quite a Palindromic String

--- Not Quite a Palindromic String/notQuiteaPalindromicString.cpp
+++ Not Quite a Palindromic String/notQuiteaPalindromicString.cpp
@@ -1,1 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        string result(n, 'a');
+        result[n - 1] = 'b';
+
+        cout << result << "\n";
+    }
+    return 0;
+}
