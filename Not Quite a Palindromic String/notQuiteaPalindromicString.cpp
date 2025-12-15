854f5f52f0035bf4d3aaf8da5e2e74a47e18638a
anikatasnin <jarintasnin09@gmail.com>
2025-12-15 23:24:43 +0600

    added Not Quite a Palindromic String

--- Not Quite a Palindromic String/notQuiteaPalindromicString.cpp
+++ Not Quite a Palindromic String/notQuiteaPalindromicString.cpp
@@ -1,37 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int t;
     cin >> t;
 
-    while(t--){
+    for (int test = 0; test < t; ++test) {
         int n, k;
         cin >> n >> k;
 
         string s;
         cin >> s;
 
-        int c0 = 0;
-        int c1 = 0;
-        for(char c : s){
-            if (c == '0') {
-                c0++;
-            }
-            else {
-                c1++;
-            }
+        int m = n / 2;
+        int z = 0;
+        for (char c : s) {
+            if (c == '0') ++z;
         }
+        int d = m - k;
+        bool possible = (z >= d) && ((z - d) % 2 == 0);
 
-        int k_max = (c0 / 2) + (c1 / 2);
-        int k_min = abs(c0 - c1) / 2;
-
-        if (k >= k_min && k <= k_max) {
-            cout << "YES\n";
+        if (possible) {
+            int a = (z - d) / 2;
+            if (k - a < 0) {
+                possible = false;
+            }
         }
-        else {
-            cout << "NO\n";
-        }
+        cout << (possible ? "YES" : "NO") << '\n';
     }
     return 0;
 }
