76e8738ed240a3f7b941fcf96a30a1bc6bfe5cd9
anikatasnin <jarintasnin09@gmail.com>
2025-12-17 20:54:55 +0600

    added Distance and Axis

--- Distance and Axis/distanceAndAxis.cpp
+++ Distance and Axis/distanceAndAxis.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+  int t;
+  cin >> t;
+
+  for (int i = 0; i < t; i++) {
+    int n, k;
+    cin >> n >> k;
+
+    int ans;
+    if (n >= k) {
+      if ((n - k) % 2 == 0) {
+        ans = 0;
+      }
+      else {
+        ans = 1;
+      }
+    }
+    else {
+      ans = k - n;
+    }
+    cout << ans << endl;
+  }
+  return 0;
+}
