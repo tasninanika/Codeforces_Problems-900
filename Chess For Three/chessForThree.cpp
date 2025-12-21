e820c0fe5e2322d850cfc58434418106369a5bbb
anikatasnin <jarintasnin09@gmail.com>
2025-12-21 18:22:58 +0600

    added Chess For Three

--- Chess For Three/chessForThree.cpp
+++ Chess For Three/chessForThree.cpp
@@ -1,1 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+  int t;
+  cin >> t;
+
+  while(t--){
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int sum = a + b + c;
+    if(sum % 2 != 0){
+      cout << -1 << '\n';
+      continue;
+    }
+
+    int ans = -1;
+
+    for(int pab = 0; pab <= a; pab++){
+      int pac = a - pab;
+      int par_ab = pab % 2;
+
+      for(int pba = par_ab; pba <= b; pba += 2){
+        int pbc = b - pba;
+        int par_ac = pac % 2;
+
+        for(int pca = par_ac; pca <= c; pca += 2){
+          int pcb = c - pca;
+
+          if((pbc + pcb) % 2 == 0){
+            int dab = min(pab, pba);
+            int dac = min(pac, pca);
+            int dbc = min(pbc, pcb);
+            int tot = dab + dac + dbc;
+            if (tot > ans) ans = tot;
+          }
+        }
+      }
+    }
+    cout << ans << '\n';
+  }
+  return 0;
+}
