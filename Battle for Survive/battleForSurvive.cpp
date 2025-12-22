7d59554b9760fdaf73cd8367c1a89c13b34dca66
anikatasnin <jarintasnin09@gmail.com>
2025-12-22 17:53:28 +0600

    added Battle for Survive

--- Battle for Survive/battleForSurvive.cpp
+++ Battle for Survive/battleForSurvive.cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+  int t;
+  cin >> t;
+
+  while(t--){
+    int n;
+    cin >> n;
+
+    vector<long long> a(n + 1);
+    long long sum = 0;
+
+    for(int i = 1; i <= n; i++){
+      cin >> a[i];
+      sum += a[i];
+    }
+
+    long long ans = sum - 2LL * a[n - 1];
+
+    cout << ans << '\n';
+  }
+  return 0;
+}
