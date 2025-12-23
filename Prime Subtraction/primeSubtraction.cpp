f1353b105eeb28d0fe9358170db2642a13c1896c
anikatasnin <jarintasnin09@gmail.com>
2025-12-23 22:43:45 +0600

    added Prime Subtraction

--- Prime Subtraction/primeSubtraction.cpp
+++ Prime Subtraction/primeSubtraction.cpp
@@ -1,1 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+  int t;
+  cin >> t;
+
+  while(t--){
+    long long x, y;
+    cin >> x >> y;
+
+    if(x - y > 1){
+      cout << "YES\n";
+    }
+    else{
+      cout << "NO\n";
+    }
+  }
+  return 0;
+}
