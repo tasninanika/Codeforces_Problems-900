97b56642fb9f56641c1410c3d66c52e9d6d505f2
anikatasnin <jarintasnin09@gmail.com>
2025-12-24 22:10:26 +0600

    added Yet Another Tetris Problem

--- Yet Another Tetris Problem/yetAnotherTetrisProblem.cpp
+++ Yet Another Tetris Problem/yetAnotherTetrisProblem.cpp
@@ -1,1 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+  int t;
+  cin >> t;
+
+  while(t--){
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for(int i = 0; i < n; i++){
+      cin >> a[i];
+    }
+
+    int par = a[0] % 2;
+    bool ok = true;
+    for(int i = 1; i < n; i++){
+      if(a[i] % 2 != par){
+        ok = false;
+        break;
+      }
+    }
+    if(ok){
+      cout << "YES\n";
+    }
+    else{
+      cout << "NO\n";
+    }
+  }
+  return 0;
+}
