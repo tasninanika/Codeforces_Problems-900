d772c82c92f3800eccb82b63dfa593860fe311cf
anikatasnin <jarintasnin09@gmail.com>
2025-12-27 12:59:18 +0600

    added Painting the Ribbon

--- Painting the Ribbon/paintingTheRibbon.cpp
+++ Painting the Ribbon/paintingTheRibbon.cpp
@@ -1,1 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+  int t;
+  cin >> t;
+
+  while(t--){
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    if((long long)m * (n - k - 1LL) >= n){
+      cout << "YES\n";
+    }
+    else {
+      cout << "NO\n";
+    }
+  }
+  return 0;
+}
