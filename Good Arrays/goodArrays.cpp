45265ee612bfdff360603da6fca7846b3beaf279
anikatasnin <jarintasnin09@gmail.com>
2025-12-29 12:31:47 +0600

    added Good Arrays

--- Good Arrays/goodArrays.cpp
+++ Good Arrays/goodArrays.cpp
@@ -1,1 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+  int t;
+  cin >> t;
+
+  while(t--){
+    int n;
+    cin >> n;
+
+    vector<long long> a(n);
+    long long S = 0;
+    int cnt1 = 0;
+
+    for(int i = 0; i < n; i++){
+      cin >> a[i];
+      S += a[i];
+      if(a[i] == 1)
+        cnt1++;
+    }
+    if(n == 1){
+      cout << "NO\n";
+    }
+    else{
+      if(S >= (long long)n + cnt1){
+        cout << "YES\n";
+      }
+      else {
+        cout << "NO\n";
+      }
+    }
+  }
+  return 0;
+}
